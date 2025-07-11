4a7388293454ac68cf6d8945391cb4aed27636a4
Catherene <swismitha@yahoo.co.in>
2025-07-11 19:09:10 +0530

    Mosified project prefix

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,7 +1,7 @@
 variable "project_prefix" {
   description = "Prefix used to name Azure resources"
   type        = string
-  default     = "multiccn"
+  default     = "cloudag"
 }
 
 variable "azure_location" {
