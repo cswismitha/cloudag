74f4607f0b888eaa9f5aeb79b99a981adaf6f67a
Catherene <swismitha@yahoo.co.in>
2025-07-21 10:55:24 +0530

    Fix build

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -49,7 +49,7 @@ variable "aws_lambda_function_name_sendmail" {
 variable "aws_sendgrid_secret_name" {
   description = "The name of the secret in AWS Secrets Manager containing the SendGrid API Key."
   type        = string
-  default     = "sendgridApikey1" 
+  default     = "sendgridApikey2" 
   sensitive   = true
 }
 
